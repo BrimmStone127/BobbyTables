f9aeb5ac468c6c982502e4dc6918204490365606
BrimmStone127 <claybrimm@gmail.com>
2018-10-29 13:49:40 -0400

    Added basic tests for EdgeTable

--- EdgeTableTest.java
+++ EdgeTableTest.java
@@ -2,6 +2,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EdgeTableTest extends EdgeTable {
-
   
+  @Before
+  public void setUp() throws Exception{
+    testObj = new EdgeConnector("1|BolognaSandwich" );
+    runner();
+  }
+
+  public void runner(){
+    getNumFigureTest();
+    getNameTest();
+  }
+
+  @Test 
+  public void getNumFigureTest(){
+    assertEquals("numFigureTest initialized to 1, expected output - 1",1,testObj.getNumFigureTest());
+  }
+
+  @Test
+  public void getNameTest(){
+    assertEquals("Name set to 'BolognaSandwich', expected output - 'BolognaSandwich'", "BolognaSandwich", testObj.getNameTest());
+  }
+
 }
