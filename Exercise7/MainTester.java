aa396f1430e65fe96c2863b803727755d85262fb
BrimmStone127 <claybrimm@gmail.com>
2018-11-05 14:51:30 -0500

    Added code to MainTester

--- Exercise7/MainTester.java
+++ Exercise7/MainTester.java
@@ -1,5 +1,28 @@
-public class MainTester{
-  public static void main(String[] args){
+import java.util.*;
+import java.io.*;
 
+public class MainTester{
+
+  public static void main(String[] args){
+    MainTester main = new MainTester();
+    main.read();
   }
+
+  public void read(){
+    Scanner scan = new Scanner(System.in);
+    String command = scan.nextLine();
+    String[] commandSplit = command.split(" ");
+
+    if(commandSplit[0].equals("-h")){
+      System.out.println("HELP");
+    }else if(commandSplit[0].equals("-n")){
+      System.out.println("TEST OBJECT");
+    }else if(commandSplit[0].equals("-f")){
+      System.out.println("TEST FILE "+commandSplit[1]);
+    }else{
+      System.out.println("Command not recognized, enter -h to view help menu");
+      read();
+    }
+  }
+
 }
