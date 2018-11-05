b092722217e9ba1487863e805cf78de9a9478089
BrimmStone127 <claybrimm@gmail.com>
2018-11-05 15:44:35 -0500

    MainTester -h command complete

--- Exercise7/MainTester.java
+++ Exercise7/MainTester.java
@@ -14,11 +14,25 @@ public class MainTester{
     String[] commandSplit = command.split(" ");
 
     if(commandSplit[0].equals("-h")){
-      System.out.println("HELP");
+      System.out.println();
+      System.out.println("************* H E L P ***************************");
+      System.out.println();
+      System.out.println();
+      System.out.println("------------ COMMANDS ---------------------------");
+      System.out.println("-n <test_object> ---------- Run tests utilizing a test object");
+      System.out.println("-f <test_file>  ----------- Run tests utilizing a test object file");
+      System.out.println("exit           ------------ Exit application");
+      System.out.println("-------------------------------------------------");
+      System.out.println();
+      read();
     }else if(commandSplit[0].equals("-n")){
-      System.out.println("TEST OBJECT");
+      System.out.println("TEST OBJECT"+commandSplit[1]);
+      read();
     }else if(commandSplit[0].equals("-f")){
       System.out.println("TEST FILE "+commandSplit[1]);
+      read();
+    }else if(commandSplit[0].equals("exit")){
+      System.exit(0);
     }else{
       System.out.println("Command not recognized, enter -h to view help menu");
       read();
